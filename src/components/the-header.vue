74e8d465a719a27b8fa31e5e6e33266d6f49c838
milos018 <milos018@gmail.com>
2021-08-16 16:55:55 +0200

    working in image

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -1,6 +1,16 @@
 <template>
   <header
-    class="bg-[#EB690A] h-[50px] lg:pl-[55px] flex items-center justify-center lg:justify-start"
+    class="
+      bg-[#EB690A]
+      shadow-lg
+      h-[50px]
+      lg:pl-[55px]
+      flex
+      items-center
+      justify-center
+      lg:justify-start
+      z-20
+    "
   >
     <router-link to="/">
       <img src="/logo.png" class="h-6" />
--- src/components/the-hero.vue
+++ src/components/the-hero.vue
@@ -1,17 +1,47 @@
 <template>
-  <section
-    class="h-96 lg:min-h-[450px] relative bg-center bg-cover"
-    :style="{ backgroundImage: `url(/slide_img_0${currentImage}-min.png)` }"
-  >
+  <section class="relative z-10">
     <div
-      class="h-auto w-full absolute top-1/3 flex flex-col gap-4 px-4 lg:px-11"
+      class="
+        w-full
+        h-64
+        md:h-72
+        lg:h-96
+        fixed
+        object-cover
+        bg-cover bg-center
+        z-0
+      "
+      :style="{
+        backgroundImage: `url('/slide_img_0${currentImage}-min.png')`,
+      }"
     >
-      <div class="w-full flex justify-between items-center">
-        <img
-          @click="switchImage('prev')"
-          src="/silder_nav_left.png"
-          class="h-full cursor-pointer"
-        />
+      <!-- <img
+        :src="`/slide_img_0${currentImage}-min.png`"
+        alt="Hero image"
+        class="w-full"
+      /> -->
+    </div>
+    <div
+      class="
+        absolute
+        top-[1.4rem]
+        sm:top-1/3
+        gap-2
+        md:gap-4
+        px-4
+        lg:px-11
+        w-full
+        flex
+        justify-between
+        items-start
+      "
+    >
+      <img
+        @click="switchImage('prev')"
+        src="/silder_nav_left.png"
+        class="h-full cursor-pointer"
+      />
+      <div class="flex flex-col items-center justify-start gap-2 md:gap-4">
         <h1
           class="
             max-w-md
@@ -19,42 +49,44 @@
             text-[#fff]
             font-bold
             leading-tight
-            text-3xl
+            text-xl
+            md:text-3xl
             lg:text-5xl
             text-center
           "
         >
           Get The Boot A Birds Eye Look Into Mcse Boot Camps
         </h1>
-        <img
-          @click="switchImage('next')"
-          src="/silder_nav_right.png"
-          class="cursor-pointer"
-        />
+        <div>
+          <router-link
+            to="/form"
+            class="
+              from-[#AF3700]
+              bg-gradient-to-r
+              to-[#EB690A]
+              text-[#fff] text-sm
+              md:text-lg
+              font-bold
+              flex
+              py-2
+              md:py-3
+              px-4
+              md:px-12
+              items-center
+              justify-center
+              tracking-wider
+            "
+          >
+            Discover
+          </router-link>
+        </div>
       </div>
-      <div class="flex justify-center">
-        <router-link
-          to="/form"
-          class="
-            from-[#AF3700]
-            bg-gradient-to-r
-            to-[#EB690A]
-            text-[#fff] text-sm
-            md:text-lg
-            font-bold
-            flex
-            py-3
-            px-12
-            items-center
-            justify-center
-            tracking-wider
-          "
-        >
-          Discover
-        </router-link>
-      </div>
+      <img
+        @click="switchImage('next')"
+        src="/silder_nav_right.png"
+        class="cursor-pointer"
+      />
     </div>
-    <!-- Discover Button -->
   </section>
 </template>
 
