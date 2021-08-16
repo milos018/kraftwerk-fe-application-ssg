5e774d2a8ec99f761915e4a1d451a62819f81766
Milos Dimitrijevic <milos018@gmail.com>
2021-08-16 02:43:16 +0200

    image opt

--- src/components/the-footer.vue
+++ src/components/the-footer.vue
@@ -1,16 +1,32 @@
 <template>
-  <section class="bg-[#ECF1F5] flex flex-col lg:flex-row gap-5 justify-between px-[49px] py-[40px]">
-    <div class="lg:w-1/2">
-      <img src="/content_img_01.png" alt="Man working on a computer" class="w-full" />
-    </div>
-    <p class="lg:w-1/2">
-      When asked, “If you could wish for one thing only, what would that wish be?” almost everyone;
-      from beauty pagent contestants, to politicians, to religious leaders, to children, to the
-      <span class="text-[#EB7722] font-bold">average person</span> on the street states, “Peace On
-      Earth” or “An end to all wars”. Those wishes, while exemplary, are meaningless. As long as
-      humans exist there will never be peace on earth. Throughout the history of humankind there has
-      never been peace on earth. Cavemen fought other cavemen over territory, food and even women.
-      Cain killed Abel over God's respect.
-    </p>
-  </section>
+	<section
+		class="
+			bg-[#ECF1F5]
+			flex flex-col
+			lg:flex-row
+			gap-5
+			justify-between
+			px-[49px]
+			py-[40px]
+		"
+	>
+		<div class="lg:w-1/2">
+			<img
+				src="/content_img_01-min.png"
+				alt="Man working on a computer"
+				class="w-full"
+			/>
+		</div>
+		<p class="lg:w-1/2">
+			When asked, “If you could wish for one thing only, what would that wish
+			be?” almost everyone; from beauty pagent contestants, to politicians, to
+			religious leaders, to children, to the
+			<span class="text-[#EB7722] font-bold">average person</span> on the street
+			states, “Peace On Earth” or “An end to all wars”. Those wishes, while
+			exemplary, are meaningless. As long as humans exist there will never be
+			peace on earth. Throughout the history of humankind there has never been
+			peace on earth. Cavemen fought other cavemen over territory, food and even
+			women. Cain killed Abel over God's respect.
+		</p>
+	</section>
 </template>
--- src/components/the-hero.vue
+++ src/components/the-hero.vue
@@ -1,63 +1,83 @@
 <template>
-  <section
-    :style="{ 'background-image': `url(/slide_img_0${currentImage}.png)` }"
-    class="w-full h-[350px] lg:h-[450px] relative px-11 bg-cover bg-center object-cover"
-  >
-    <div class="w-full h-full z-10 flex flex-col justify-center items-center">
-      <div class="w-full flex justify-between items-center">
-        <img
-          @click="switchImage('prev')"
-          src="/silder_nav_left.png"
-          class="cursor-pointer hover:scale-105 duration-300"
-        />
-        <h1
-          class="max-w-md lg:max-w-3xl text-[#fff] font-bold leading-tight text-lg md:text-3xl lg:text-5xl text-center"
-        >
-          Get The Boot A Birds Eye Look Into Mcse Boot Camps
-        </h1>
-        <img
-          @click="switchImage('next')"
-          src="/silder_nav_right.png"
-          class="cursor-pointer hover:scale-105 duration-300"
-        />
-      </div>
-      <router-link
-        to="/form"
-        class="
-          absolute
-          bottom-16
-          lg:bottom-24
-          w-48
-          lg:w-56
-          h-12
-          lg:h-[50px]
-          bg-gradient-to-r
-          from-[#AF3700]
-          to-[#EB690A]
-          text-[#fff] text-sm
-          md:text-lg
-          font-bold
-          flex
-          items-center
-          justify-center
-          tracking-wider
-        "
-      >
-        Discover
-      </router-link>
-    </div>
-  </section>
+	<section
+		:style="{ 'background-image': `url(/slide_img_0${currentImage}-min.png)` }"
+		class="
+			w-full
+			h-[350px]
+			lg:h-[450px]
+			relative
+			px-4
+			lg:px-11
+			bg-cover bg-center
+			object-cover
+		"
+	>
+		<div class="w-full h-full z-10 flex flex-col justify-center items-center">
+			<div class="w-full flex justify-between items-center">
+				<img
+					@click="switchImage('prev')"
+					src="/silder_nav_left.png"
+					class="cursor-pointer hover:scale-105 duration-300"
+				/>
+				<h1
+					class="
+						max-w-md
+						lg:max-w-3xl
+						text-[#fff]
+						font-bold
+						leading-tight
+						text-2xl
+						md:text-3xl
+						lg:text-5xl
+						text-center
+					"
+				>
+					Get The Boot A Birds Eye Look Into Mcse Boot Camps
+				</h1>
+				<img
+					@click="switchImage('next')"
+					src="/silder_nav_right.png"
+					class="cursor-pointer hover:scale-105 duration-300"
+				/>
+			</div>
+			<!-- Discover Button -->
+			<router-link
+				to="/form"
+				class="
+					absolute
+					bottom-16
+					lg:bottom-24
+					w-48
+					lg:w-56
+					h-12
+					lg:h-[50px]
+					bg-gradient-to-r
+					from-[#AF3700]
+					to-[#EB690A]
+					text-[#fff] text-sm
+					md:text-lg
+					font-bold
+					flex
+					items-center
+					justify-center
+					tracking-wider
+				"
+			>
+				Discover
+			</router-link>
+		</div>
+	</section>
 </template>
 
-<script setup>
-import { ref, computed } from "vue";
-
+<script setup lang="ts">
 const currentImage = ref(1);
-const switchImage = (go) => {
-  if (go === "next" && currentImage.value === 3) return (currentImage.value = 1);
-  if (go === "prev" && currentImage.value === 1) return (currentImage.value = 3);
-  if (go === "next") currentImage.value += 1;
-  if (go === "prev") currentImage.value -= 1;
+const switchImage = (go: String) => {
+	if (go === 'next' && currentImage.value === 3)
+		return (currentImage.value = 1);
+	if (go === 'prev' && currentImage.value === 1)
+		return (currentImage.value = 3);
+	if (go === 'next') currentImage.value += 1;
+	if (go === 'prev') currentImage.value -= 1;
 };
 </script>
 
